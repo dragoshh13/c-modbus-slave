8ec99103fd0c2d954ad45b3890f2c1b80c61fccb
Jonas Almaas <jonas.almaas@siemens-energy.com>
2025-09-03 10:11:57 +0200

    Rename test_info struct to test_info_s
    This follows our naming convention

--- test/src/test_lib.h
+++ test/src/test_lib.h
@@ -78,22 +78,22 @@
 #define EXPECT_EQ(expect, actual) INTERNAL_ASSERT_EQ(expect, actual, 0)
 #define ASSERT_EQ(expect, actual) INTERNAL_ASSERT_EQ(expect, actual, 1)
 
-struct test_info {
+struct test_info_s {
 	void (*fn)(int *test_failed);
 	const char *name;
 };
 
 #define TEST(name) \
 	static void name##_fn(int *test_failed); \
-	static const struct test_info name = {name##_fn, #name}; \
+	static const struct test_info_s name = {name##_fn, #name}; \
 	static void name##_fn(int *test_failed)
 
 #define TEST_MAIN(...) \
 	int main(void) \
 	{ \
-		const struct test_info tests[] = {__VA_ARGS__}; \
+		const struct test_info_s tests[] = {__VA_ARGS__}; \
 		int i, test_failed, failed_tests=0; \
-		for (i=0; i < sizeof tests / sizeof(struct test_info); ++i) { \
+		for (i=0; i < sizeof tests / sizeof(struct test_info_s); ++i) { \
 			test_failed = 0; \
 			tests[i].fn(&test_failed); \
 			if (test_failed) ++failed_tests; \
